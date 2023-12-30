571debdc0e58664c46efaaa97328fb6d065d528f
Gurupreeth <igurupreet@gmail.com>
2023-12-31 00:59:20 +0530

    added the if_conditional class

---
+++ Core_java_topics/src/o2_conditional_statements/If_condition.java
@@ -0,0 +1,16 @@
+package o2_conditional_statements;
+
+public class If_condition {
+
+	public static void main(String[] args) {
+		int a = 100; int b = 20;
+		
+		if (a< b)
+		{
+			System.out.println( a+ " is smaller than " + b);
+		}
+		else {
+			System.out.println(b + " is smaller than "+ a);
+		}
+	}
+}
