04ae2e61371ddf2c3cb69084940a8b184af19e9f
Gurupreeth <igurupreet@gmail.com>
2023-12-31 00:27:16 +0530

    java project added with O1_Datatypes.java file

---
+++ Core_java_topics/src/o1_intro/O1_Datatypes.java
@@ -0,0 +1,30 @@
+package o1_intro;
+
+public class O1_Datatypes {
+
+	public static void main(String[] args)
+	{
+		// Literals : - there are 4 types of literals in java 
+	    //1. Number literal - integers and decimal numbers. 
+		System.out.println(10);
+		System.out.println(2.34);
+		
+		// 2. boolean literals. 
+		System.out.println(true);
+        System.out.println(false);
+        
+        // 3. char literals
+        System.out.println('a');
+        System.out.println('2');
+        
+        // string literals
+        System.out.println("hello");
+        System.out.println("1234");
+        System.out.println("abc123");
+        System.out.println("@#$@#$@#$@$");
+        System.out.println("welcome to java");
+        System.out.println("hello 'ecoders' welcome to java");
+        System.out.println("hello ecoders \'welcome\' to java");
+	}
+
+}
