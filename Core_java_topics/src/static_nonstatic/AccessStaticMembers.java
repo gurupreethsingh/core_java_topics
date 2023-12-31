9b27e50ca65894b0375a07cc7d7a28bff2f6973b
Gurupreeth <igurupreet@gmail.com>
2023-12-31 12:10:41 +0530

    i just added static and non static access program.

---
+++ Core_java_topics/src/static_nonstatic/AccessStaticMembers.java
@@ -0,0 +1,13 @@
+package static_nonstatic;
+
+public class AccessStaticMembers {
+
+	static int a = 20;
+	public static void main(String[] args)
+	{
+		// accessing static global members in the same class. 
+		System.out.println(a);
+
+	}
+
+}
